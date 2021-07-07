bec958a6b2fa9f6fd81b402ca5cac39b05f2974a
Jheyki <67764506+Jheyki@users.noreply.github.com>
2021-07-07 10:17:12 +0800

    comment part 3

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function sendMail(params){
         console.log("success",res.status)
     })
 }
+
+//function sendMail(params){
+                    
+//    var tempParams = {
+    
+
+//    from_name:document.getElementById("fromName").value,
+//    to_name:document.getElementById("toName").value,
+    //message:document.getElementById("msg").value,
+//    }
+
+//    emailjs.send('kapoycoffe62@gmail.com','KapoyCoffee62', tempParams)
+//   .then(function(res){
+//  console.log("success",res.status)
+//     })
+//   }
